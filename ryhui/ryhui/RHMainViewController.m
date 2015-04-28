b20401668845ae43015af3217fe2d8d1760655a7
jufenghudong <jufenghudong@jufenghongdeMBP.lan>
2015-04-28 20:17:46 +0800

    update

--- ryhui/ryhui/RHMainViewController.m
+++ ryhui/ryhui/RHMainViewController.m
@@ -89,6 +89,8 @@
 
 -(void)refesh
 {
+    [self.segment1Array removeAllObjects];
+    [self.segment2Array removeAllObjects];
     [self segment1Post];
     [self segment2Post];
     [self getSegmentnum1];
@@ -229,24 +231,29 @@
     switch (index) {
         case 0:
             self.contentLabel.text=@"       借款方为经营良好的中小微企业及个体工商户。为保障投资人权益，融益汇通过评级严格筛选合作机构。所有借款项目均由合作机构评审后推荐，并经融益汇多轮再评审后发布。所有项目均由合作机构提供全额本息担保。";
-            if ([self.segment1Array count]<=0) {
-                [self segment1Post];
-            }else{
-                [self.dataArray removeAllObjects];
-                [self.dataArray addObjectsFromArray:self.segment1Array];
-                [self.tableView reloadData];
-            }
+//            if ([self.segment1Array count]<=0) {
+//                [self segment1Post];
+//            }else{
+//                [self.dataArray removeAllObjects];
+//                [self.dataArray addObjectsFromArray:self.segment1Array];
+//                [self.tableView reloadData];
+//            }
+            
+            [self.segment1Array removeAllObjects];
+            [self segment1Post];
             break;
         case 1:
             self.contentLabel.text=@"       借款方为接受就业培训的在读学生，贷款用于支付就业培训费用。培训机构承诺为学生就业提供保障，并承担未就业学生的全部还款本息；同时融益汇从每笔助学贷款的服务费中提取一定比例的资金作为助学贷专项风险保障金以备代偿。通过就业担保和风险保障金双重本息保障机制为您的投资保驾护航。";
 
-            if ([self.segment2Array count]<=0) {
-                [self segment2Post];
-            }else{
-                [self.dataArray removeAllObjects];
-                [self.dataArray addObjectsFromArray:self.segment2Array];
-                [self.tableView reloadData];
-            }
+//            if ([self.segment2Array count]<=0) {
+//                [self segment2Post];
+//            }else{
+//                [self.dataArray removeAllObjects];
+//                [self.dataArray addObjectsFromArray:self.segment2Array];
+//                [self.tableView reloadData];
+//            }
+            [self.segment2Array removeAllObjects];
+            [self segment2Post];
             break;
         default:
             break;
