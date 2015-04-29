a36c7846ad45906ed1bb9d4b745cefc704ba485b
jufenghudong <jufenghudong@jufenghudongdeMacBook-Pro.local>
2015-04-29 11:19:05 +0800

    update

--- ryhui/ryhui/RHWithdrawViewController.m
+++ ryhui/ryhui/RHWithdrawViewController.m
@@ -30,7 +30,7 @@
     self.changeCardsButton.layer.cornerRadius=9;
     self.changeCardsButton.layer.masksToBounds=YES;
     [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(textFieldTextDidChange:) name:UITextFieldTextDidChangeNotification object:nil];
-    self.withdrawTF.enabled=YES;
+    self.withdrawTF.enabled=NO;
     self.cardsView.hidden=YES;
 }
 //balance = "80.29000000000001";
@@ -60,7 +60,7 @@
         
         if ((cards&&[cards count]>0)||(qpCard&&[qpCard count]>0)) {
             self.overView.hidden=YES;
-            self.withdrawTF.enabled=NO;
+            self.withdrawTF.enabled=YES;
         }else{
             self.overView.hidden=NO;
         }
